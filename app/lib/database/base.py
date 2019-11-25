050a5493decf03808337079b1f0343728deb6719
ray3175 <57464715@qq.com>
2019-11-26 01:03:27 +0800

    新增modules dict方法，导航条样式改动

--- app/lib/database/base.py
+++ app/lib/database/base.py
@@ -3,7 +3,15 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-Base.to_dict = lambda: None
+Base.dict = lambda x: recursive_func(x)
 
 
+def recursive_func(x):
+    _return = dict()
+    for i, j in x.__dict__.items():
+        if not i.startswith("_"):
+            if isinstance(j, Base):
+                j = recursive_func(j)
+            _return.update({i: j})
+    return _return
 
