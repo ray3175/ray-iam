d935821b0147df1fbc932e601a021081a44a0a7a
ray3175 <57464715@qq.com>
2020-03-14 03:15:25 +0800

    增加project缓存

--- app/lib/cache/redis.py
+++ app/lib/cache/redis.py
@@ -5,6 +5,9 @@ from ...config import AppConfig
 
 class Redis(ABC):
     redis_config = AppConfig()["cache"]["redis"]
+    project_redis_config = redis_config["project-db"]
+    project_pool = redis.ConnectionPool(password=redis_config["password"], host=redis_config["host"], port=redis_config["port"], db=project_redis_config["db"])
+    project_redis = redis.Redis(connection_pool=project_pool)
     auth_redis_config = redis_config["auth-db"]
     auth_pool = redis.ConnectionPool(password=redis_config["password"], host=redis_config["host"], port=redis_config["port"], db=auth_redis_config["db"])
     auth_redis = redis.Redis(connection_pool=auth_pool)
--- app/service/auth.py
+++ app/service/auth.py
@@ -1,3 +1,4 @@
+from ..lib.cache import Cache
 from ..lib.database import DB
 from . import Service
 from .administrator import ServiceAdministrator
@@ -15,5 +16,9 @@ class ServiceAuth(Service):
         return ServiceAdministrator().ray_iam_max_auth(account, auth)
 
     @DB.transaction(auto_commit=False)
-    def project_iam_auth(self, name, auth_code, **kwargs):
-        return ServiceProject().project_iam_auth(name, auth_code, **kwargs)
+    def project_iam_auth_from_DB(self, name, auth_code, **kwargs):
+        return (project:=ServiceProject().project_iam_auth(name, auth_code, **kwargs)) and project()
+
+    @Cache.cache_redis("project", "name")
+    def project_iam_auth(self, name, auth_code):
+        return self.project_iam_auth_from_DB(name, auth_code)
