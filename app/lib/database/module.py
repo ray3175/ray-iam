8007ca5da489d4648e185fe20cf57e716b027cd7
ray3175 <57464715@qq.com>
2020-03-22 23:25:33 +0800

    3.8特性

--- app/lib/database/module.py
+++ app/lib/database/module.py
@@ -12,7 +12,7 @@ def recursive_to_dict(x, *args, **kwargs):
         if not (i.startswith("_") or (kwargs.get("del_column") and i in kwargs["del_column"])):
             _return.update({i: j})
     for i in kwargs.get("add_column", []).copy():
-        if y:=(getattr(x, i, None) or getattr(x, f"{i}_from_{x.__tablename__}", None)):
+        if y:=getattr(x, i, None) or getattr(x, f"{i}_from_{x.__tablename__}", None):
             kwargs["add_column"].remove(i)
             if isinstance(y, Module):
                 y = y(add_column=kwargs["add_column"].copy(), del_column=kwargs.get("del_column"))
