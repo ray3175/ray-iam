f3d385b9fde502c106ed9c62f0f85f7c9077b4b8
ray3175 <57464715@qq.com>
2020-05-12 16:45:37 +0800

    去掉openid的索引

--- gui/src/modules/we_chat/user.py
+++ gui/src/modules/we_chat/user.py
@@ -7,7 +7,7 @@ class WeChatUser(Module):
     __tablename__ = "we_chat_user"
 
     id = Column(BigInteger, primary_key=True, comment="微信用户ID")
-    openid = Column(String(256), index=True, nullable=False, comment="微信openid")
+    openid = Column(String(256), nullable=False, comment="微信openid")
     person_id = Column(BigInteger, ForeignKey("person.id", ondelete="SET NULL"), index=True, comment="用户ID")
     person = relationship("Person", backref="we_chat_user_from_person")
     xy = Column(Boolean, default=True, comment="软删除")
