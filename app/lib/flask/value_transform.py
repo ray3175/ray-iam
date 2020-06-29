b354d926d0d5f756f79da5cbef8d1f8d95965777
ray3175 <57464715@qq.com>
2020-06-30 00:25:44 +0800

    修改ValueTransform策略

--- app/lib/flask/value_transform.py
+++ app/lib/flask/value_transform.py
@@ -1,11 +1,11 @@
 class ValueTransform:
     @staticmethod
     def intstr2int(int_string):
-        return int(int_string) if isinstance(int_string, str) and int_string.isdigit() else None
+        return int(int_string) if (isinstance(int_string, str) and int_string.isdigit()) or isinstance(int_string, (int, float)) else None
 
     @staticmethod
     def floatstr2float(float_string):
-        return float(float_string) if all([int_string.isdigit() for int_string in float_string.split(".")]) else None
+        return float(float_string) if (isinstance(float_string, str) and all([int_string.isdigit() for int_string in float_string.split(".")])) or isinstance(float_string, (int, float)) else None
 
     @staticmethod
     def boolstr2bool(bool_string):
