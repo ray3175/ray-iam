81c0815338e2c18ec6fa4cb69668b831f85eb198
ray3175 <57464715@qq.com>
2020-11-16 02:38:17 +0800

    更新Hash

--- app/common/hash.py
+++ app/common/hash.py
@@ -1,10 +1,16 @@
 from xy.cipher.hash import Hash
+from xy.cipher.code import Code
 from ..config import AppConfig
 
 
-class RayIamHash(Hash):
+class RayIamHash(Hash, Code):
     hash_config = AppConfig().app["hash"]
 
     def __init__(self, hash_type=hash_config["hash_type"], salt=hash_config["salt"], iterations=hash_config["iterations"]):
-        super().__init__(hash_type, salt, iterations)
+        super(RayIamHash, self).__init__(hash_type, salt, iterations)
+        super(Hash, self).__init__("hex")
+
+    def encrypt(self, text):
+        hash_bytes = super().encrypt(text.encode("utf-8"))
+        return super().encode(hash_bytes).decode("utf-8")
 
